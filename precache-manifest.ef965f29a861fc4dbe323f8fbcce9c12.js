4e8bbf2ba141938c82258208381aca25f2b9fb25
narendrakumar14-R <narendrarakumarr2023@gmail.com>
2025-05-31 02:11:12 -0500

    Updates

---
+++ precache-manifest.ef965f29a861fc4dbe323f8fbcce9c12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c40bb92147d9c824cc2dd6f6ab13e6",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "9f1deafa2521f3b4f93b",
+    "url": "/react-demo/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/react-demo/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/react-demo/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "9f1deafa2521f3b4f93b",
+    "url": "/react-demo/static/js/main.97ce6228.chunk.js"
+  },
+  {
+    "revision": "ede912997875b54d1c77",
+    "url": "/react-demo/static/js/runtime~main.6af32c67.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-demo/static/media/logo.06e73328.svg"
+  }
+]);
